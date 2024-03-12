fda2e07fcc5a4334863a80d21dfa3e9e090e1851
Caden Upson <cadenupson@gmail.com>
2024-03-12 12:34:23 -0700

    Normalize database

--- backend/index.js
+++ backend/index.js
@@ -104,7 +104,7 @@ app.get("/ingredients/full", (request, response) => {
     sugar_amt, fat_amt, protein_amt, carb_amt,
     is_bitter, is_sweet, is_salty, is_sour, is_umami
     FROM INGREDIENT
-    JOIN RESTRICTION ON INGREDIENT.ingredient_name = RESTRICTION.ingredient_name
+    JOIN RESTRICTION ON INGREDIENT.ingredient_type = RESTRICTION.ingredient_type
     JOIN NUTRITION ON INGREDIENT.ingredient_name = NUTRITION.ingredient_name
     JOIN FLAVORPROFILE ON INGREDIENT.ingredient_name = FLAVORPROFILE.ingredient_name
   `;
